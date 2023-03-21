24f7746e87dfb9dbeee0526ddf19d75df0838c43
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-20 22:16:49 -0700

    made the timer work bro !!!

--- Develop/script.js
+++ Develop/script.js
@@ -9,11 +9,16 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
+
+
   // If the click listener function is defined as a regular function, then
   // this will be set to the DOM element that triggered the event (i.e.,
   // the element that was clicked).
   // However, if the function is called using the addEventListener method,
-  // then this inside the function will refer to the DOM element to which the event listener was attached.
+  // then this inside the function will refer to the DOM element to which
+  // the event listener was attached.
+
+
   
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -22,30 +27,21 @@ $(function () {
   // current hour in 24-hour time?
 
 
-  
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
 
   
-  // TODO: Add code to display the current date in the header of the page.
-  // alert(new Date($.now()));
-
-  var currentdate = new Date(); 
-  var datetime = "it is " + currentdate.getDate() + "/"
-    + (currentdate.getMonth()+1)  + "/" 
-    + currentdate.getFullYear() + " at "  
-    + currentdate.getHours() + ":"  
-    + currentdate.getMinutes()
-  console.log(datetime);
+  // Add code to display the current date in the header of the page.
+var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
 });
-
-
 
 // THEN the current day is displayed at the top of the calendar
 // THEN I am presented with time blocks for standard business hours
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // THEN I can enter an event
-// THEN the text for that event is saved in local storage
-// THEN the saved events persist
+// THEN the text for that event is saved in local storage;
+// THEN the saved events persist;
