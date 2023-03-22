9c1909c53249c21b2f36dff4bd4ce8d1f4d1c75c
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-22 08:37:04 -0700

    testing other peoples' code

--- Develop/script.js
+++ Develop/script.js
@@ -9,12 +9,32 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-  $('.saveBtn').click(function(){
-    console.log("hello world!")
-  });
-  document.getElementsByClassName("saveBtn").onclick = function(){
-    
-  };
+          // $('.saveBtn').click(function(){
+          //   console.log("hello world!")
+          // });
+          // document.getElementsByClassName("saveBtn").onclick = function(){
+          //   console.log("hello creature!")
+          // };
+
+  $(".saveBtn").on("click", function () {
+    //get nearby values.
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    //set items in local storage.
+    localStorage.setItem(time, text);
+});
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
   // If the click listener function is defined as a regular function, then
   // this will be set to the DOM element that triggered the event (i.e.,
