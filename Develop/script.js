5bfd1379b3d897622522da106275c6e7d571aa26
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-23 13:20:10 -0700

    added maybe helpful resources

--- Develop/script.js
+++ Develop/script.js
@@ -43,6 +43,9 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
+  // https://stackoverflow.com/questions/59995703/trying-to-change-background-color-based-off-if-the-hour-is-in-the-past-current
+  // https://github.com/EdenKhaos/05-work-day-scheduler/blob/master/script.js
+
   const dt = new Date();
   const time = dt.getHours();
 
